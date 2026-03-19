ffa445388911558625c1b1959b001b142ede18f3
sas765 <sasu.hallivuori@gmail.com>
2026-03-19 16:26:11 +0200

    Add new entry form, unify language

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_entry")
+def new_entry():
+    return render_template("new_entry.html")
+
+@app.route("/create_entry", methods=["POST"])
+def create_entry():
+    title = request.form["title"]
+    artist = request.form["artist"]
+    comment = request.form["comment"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO Entries (title, artist, comment, user_id)
+            VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, artist, comment, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -23,16 +40,16 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
+        return "ERROR: passwords don't match"
     password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        sql = "INSERT INTO Users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "ERROR: username already exists"
 
-    return "Tunnus luotu"
+    return "Account created successfully"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -43,16 +60,19 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
-
+        sql = "SELECT id, password_hash FROM Users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
-            return "VIRHE: väärä tunnus tai salasana"
+            return "ERROR: wrong password or usernam"
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
-CREATE TABLE users (
+CREATE TABLE Users (
     id INTEGER PRIMARY KEY,
     username TEXT UNIQUE,
     password_hash TEXT
+);
+
+CREATE TABLE Entries (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    artist TEXT,
+    comment TEXT,
+    user_id INTEGER REFERENCES Users
 );
